1bbfa59211b26b41e79eab275c55169f9db3dfdd
Yuri Ritvin <yuri1992@gmail.com>
2015-06-26 22:21:42 +0300

    init

---
+++ application/controllers/Simplelogin.php
@@ -0,0 +1,68 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Simplelogin extends CI_Controller {
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->helper('cookie');
+		$this->load->helper('form');
+		$this->load->library('form_validation');
+
+		$this->load->model('login_model');
+	}
+	private function base_template($templates_to_load) {
+		$this->load->view('base_header.php');
+		#$this->load->view('dash/menu.php');
+		foreach ($templates_to_load as $template_path=>$template_data); {
+			$this->load->view($template_path,$template_data);
+		}
+		$this->load->view('base_footer.php');
+	}
+	public function index() {
+		$this->login();
+	}
+	public function login() {
+		on_success_session();
+		$data = array();
+
+		$this->form_validation->set_rules('email', 'Email Adress', 'trim|required|valid_email');
+		$this->form_validation->set_rules('password', 'Password', 'trim|required');
+
+		if ($this->form_validation->run())
+		{
+			$data['email'] = $this->input->post('email');
+			$data['password_enc'] = sha1($this->input->post('password'));
+			if ($this->customers_model->check_customer_login($data)) {
+				$this->set_session_login($data);
+				redirect('/user/panel');
+			}
+		} 
+
+		$this->error_log = $this->lang->line('login_faild');
+		$arr = array('customers/login.php'=>$data);
+		$this->base_template($arr);
+	}
+	public function logout() {
+		$this->unset_session_login();
+		$this->index();
+		return "";	
+	}
+	private function set_session_login($data) {
+		$newdata = array(
+	        'email'  => $data['email'],
+	        'password' => $data['password_enc'],
+	        'date' => date('d-m-Y'),
+	        'logged_in' => TRUE
+		);
+		$this->session->set_userdata($newdata);
+	}
+	private function unset_session_login() {
+		$this->session->unset_userdata('email');
+		$this->session->unset_userdata('logged_in');
+		$this->session->unset_userdata('password');
+		$this->session->unset_userdata('date');
+		$this->session->sess_destroy();
+	}
+
+}
---
+++ application/helpers/login_helper.php
@@ -0,0 +1,65 @@
+<?php
+	function internal_errors() {
+		$ci =& get_instance();
+		if (isset($ci->error_log))
+			return "<div class='error'>".$ci->error_log."</div>";
+		return '';
+	}
+	function validation_errors($prefix = '<li>', $suffix = '</li>')
+	{
+		if (FALSE === ($OBJ =& _get_validation_object()))
+		{
+			return '';
+		}
+		$str = $OBJ->error_string($prefix, $suffix);
+		if ($str)
+			return '<div class="error"><ul>'.$OBJ->error_string($prefix, $suffix).'</ul></div>';
+		return internal_errors();
+	}
+	function succes_errors() {
+		//trying to get the succes erros and display;
+		$ci =& get_instance();
+		if (isset($ci->success_log))
+			return "<div class='success'>".$ci->success_log."</div>";
+		return '';
+	}
+	function __logged_in() {
+		$CI =& get_instance();
+		if (isset($_SESSION['logged_in']) && $CI->session->userdata['logged_in']) {
+	    	$data['email'] = $CI->session->userdata('email');
+	    	$data['password_enc'] = $CI->session->userdata('password');
+	 		$stat = $CI->customers_model->check_customer_login($data);
+	 		if ($stat) {
+	 			$CI->current_user = $stat;
+		 		return true;
+	 		} 
+	 	}
+	 	return false;
+	}
+	function on_invalid_session($redirect='/customers/login') {
+		$CI =& get_instance();
+		$stat = false;
+	 	if (!__logged_in()) {
+			$CI->session->sess_destroy();
+	 		redirect($redirect);
+	 		return false;
+	 	}
+	 	return true;
+	}
+	function on_success_session($redirect='/customers/customer_panel') {
+		$stat = false;
+		echo __logged_in();
+	 	if (__logged_in()) {
+	 		redirect($redirect);
+	 		return true;
+	 	}
+	 	return false;
+	}
+
+	function _t($key){
+		$CI =& get_instance();
+		return $CI->lang->line($key);
+	}
+
+
+?>
